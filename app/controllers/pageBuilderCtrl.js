31e4fcb72c69e6301578698bc2fae66bc3415df5
Dnister Lark <dnister.lark@gmail.com>
2014-12-12 21:30:28 +0200

    click on tool provide gragable element

--- app/controllers/pageBuilderCtrl.js
+++ app/controllers/pageBuilderCtrl.js
@@ -1,9 +1,10 @@
-function pageBuilderCtrl($scope) {
+function pageBuilderCtrl($scope, $compile) {
 
 	$scope.textBlockParams = {
 		width  : "250px",
 		height : "120px"
 	}
+
 
 	$scope.toolTextBlock = function (params1) {
 		var tool = {} ;
@@ -14,5 +15,10 @@ function pageBuilderCtrl($scope) {
 
 	var renderTool = function (tool) {
 		console.log("render");
+
+		var page = $('#page'),
+			template = "<div draggable class='toolTextBlock' style='width:{{tool.params.width}}; height: {{tool.params.height}}' ></div>";
+
+		angular.element(page).append($compile(template)($scope));
 	}
 }
--- app/directives/toolsDrctv.js
+++ app/directives/toolsDrctv.js
@@ -1,4 +1,43 @@
-pageBuilder.directive("tool", function(){
+// made element draggabele. Use: <element dragable></element>
+pageBuilder.directive('draggable', ['$document', function($document) {
+	return function(scope, element, attr) {
+		var startX = 0, startY = 0, x = 0, y = 0;
+
+		element.css({
+		 position: 'relative',
+		 cursor: 'pointer'
+		});
+
+		element.on('mousedown', function(event) {
+			// Prevent default dragging of selected content
+			event.preventDefault();
+
+			startX = event.pageX - x;
+			startY = event.pageY - y;
+			$document.on('mousemove', mousemove);
+			$document.on('mouseup', mouseup);
+		});
+
+		function mousemove(event) {
+			y = event.pageY - startY;
+			x = event.pageX - startX;
+
+			element.css({
+				top: y + 'px',
+				left:  x + 'px'
+			});
+		}
+
+		function mouseup() {
+			$document.off('mousemove', mousemove);
+			$document.off('mouseup', mouseup);
+		}
+	};
+}]);
+
+
+// tool directive functionality. Use: <tool></tool>
+pageBuilder.directive("tool", function($compile){
 	var linkFn = function(scope, element, attrs) {       
 		
 		var button = element.find('button');        
@@ -15,36 +54,4 @@ pageBuilder.directive("tool", function(){
 	}
 });
 
-pageBuilder.directive('draggable', ['$document', function($document) {
-	return function(scope, element, attr) {
-	var startX = 0, startY = 0, x = 0, y = 0;
 
-	element.css({
-	 position: 'relative',
-	 cursor: 'pointer'
-	});
-
-	element.on('mousedown', function(event) {
-		// Prevent default dragging of selected content
-		event.preventDefault();
-		startX = event.pageX - x;
-		startY = event.pageY - y;
-		$document.on('mousemove', mousemove);
-		$document.on('mouseup', mouseup);
-	});
-
-	function mousemove(event) {
-		y = event.pageY - startY;
-		x = event.pageX - startX;
-		element.css({
-		top: y + 'px',
-		left:  x + 'px'
-		});
-	}
-
-	function mouseup() {
-		$document.off('mousemove', mousemove);
-		$document.off('mouseup', mouseup);
-	}
-	};
-}]);
