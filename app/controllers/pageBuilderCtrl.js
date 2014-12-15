d4ca2119fc561f16934b89885435fcca7f2fca22
Dnister Lark <dnister.lark@gmail.com>
2014-12-15 16:42:47 +0200

    render simple tool

--- app/controllers/pageBuilderCtrl.js
+++ app/controllers/pageBuilderCtrl.js
@@ -15,9 +15,17 @@ function pageBuilderCtrl($scope, $compile) {
 
 	var renderTool = function (tool) {
 		console.log("render");
+		
+		tool.params.backgroundColor = ""
+
+		var letters = '0123456789ABCDEF'.split('');
+    	var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
 
 		var page = $('#page'),
-			template = "<div draggable class='toolTextBlock' style='width:{{tool.params.width}}; height: {{tool.params.height}}' ></div>";
+			template = "<div draggable class='toolTextBlock' style='background-color: "+color+"; width:"+tool.params.width+"; height: "+tool.params.height+"' ></div>";
 
 		angular.element(page).append($compile(template)($scope));
 	}
