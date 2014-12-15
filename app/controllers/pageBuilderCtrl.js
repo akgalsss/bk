b4a4bf5497721aef9d7dc5b646c5039f88e15c13
Dnister Lark <dnister.lark@gmail.com>
2014-12-15 18:31:11 +0200

    style fixing

--- app/controllers/pageBuilderCtrl.js
+++ app/controllers/pageBuilderCtrl.js
@@ -3,17 +3,8 @@ function pageBuilderCtrl($scope, $compile, $templateCache, $http) {
 	// render and display tool 
 	var renderTool = function (tool) {
 
-		//--  mockup color generator
-		var letters = '0123456789ABCDEF'.split(''),
-			color = '#';
-
-		for (var i = 0; i < 6; i++ ) {
-			color += letters[Math.floor(Math.random() * 16)];
-		} 
-		//-- end mockup
-
 		var page = $('#page'),
-			template = "<div draggable class='toolTextBlock' style='background-color: "+color+"; width:"+tool.width+"; height: "
+			template = "<div draggable class='"+tool.class+"' style='background-color: "+((tool.color)?tool.color:"")+"; width:"+tool.width+"; height: "
 			+tool.height+"'>"+tool.data+"</div>";
 
 		angular.element(page).append($compile(template)($scope));
@@ -23,8 +14,18 @@ function pageBuilderCtrl($scope, $compile, $templateCache, $http) {
 	$scope.toolTextBlock = function () {
 		var tool = {} ;
 
+		//--  mockup color generator
+		var letters = '0123456789ABCDEF'.split(''),
+			color = '#';
+
+		for (var i = 0; i < 6; i++ ) {
+			color += letters[Math.floor(Math.random() * 16)];
+		} 
+		//-- end mockup
+
 		$http.get('/data/textBlockTool.json').success(function(data) {
 			tool = data;
+			tool.color = color;
 			renderTool(tool);
 		}).
 		error(function(data, status, headers, config) {
