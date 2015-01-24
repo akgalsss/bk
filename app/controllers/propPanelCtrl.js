b37284f86ec066e13817bd179a67e69071537e81
Dnister Lark <dnister.lark@gmail.com>
2015-01-24 18:26:25 +0200

    propPanel: add rgbToHex. Implement correct setting color value in data picker.

--- app/controllers/propPanelCtrl.js
+++ app/controllers/propPanelCtrl.js
@@ -18,20 +18,36 @@ function propPanelCtrl($scope, $compile, propPanelServ) {
 			activeTool = angular.element('.activeTool'),
 			rows;
 
-		activeTool = angular.element(activeTool).toArray();
 
 		var createPropertyRow = function (prop, val, inputType) {
 			inputType = inputType || 'text';
 			min = (inputType == 'number') ? "min='50'" : "";
 
-			return "<tr><td class='prop'>"+prop+":</td><td class='value'><input type='"+inputType+"' "+min+" value='"+val+"'/></td></tr>";
+			return "<tr><td class='prop'>"+prop+":</td><td class='value'><input ng-change='updatePageView(\"width\")' type='"+inputType+"' "+min+" value='"+val+"'/></td></tr>";
 		}
 
+
+		// convert 'rgb(255,255,255)' string to #ffffff color
+		var rgbStringToHex = function (rgbString) {
+			var a = rgbString.split("(")[1].split(")")[0];
+				a = a.split(",");
+
+			var b = a.map(function(x) {					//For each array element
+					x = parseInt(x).toString(16);		//Convert to a base16 string
+
+					return (x.length==1) ? "0"+x : x;	//Add zero if we get only one character
+				});
+
+			return "#"+b.join("");
+		}
+
+
+		activeTool = activeTool.toArray();
 
 		if (activeTool[0]['attributes']['class']['nodeValue'].indexOf("toolTextBlock") > -1) {
 			rows = createPropertyRow("width",parseInt(activeTool[0]['style']['width']), 'number');
 			rows += createPropertyRow("height",parseInt(activeTool[0]['style']['height']), 'number');
-			rows += createPropertyRow("backgroundColor",activeTool[0]['style']['background-color']);
+			rows += createPropertyRow("backgroundColor",rgbStringToHex(activeTool[0]['style']['background-color']), 'color');
 		}
 
 		if (activeTool[0]['attributes']['class']['nodeValue'].indexOf("toolImageBlock") > -1) {
@@ -41,6 +57,11 @@ function propPanelCtrl($scope, $compile, propPanelServ) {
 
 		angular.element(propTable).html($compile(rows)($scope));
 
+	}
+
+	// update page when change style properth
+	$scope.updatePageView = function () {
+		var activeTool = angular.element('.activeTool');
 	}
 
 
