bc0c126a7216994cea2ba2f7f172c7e76c44f7f8
Dnister Lark <dnister.lark@gmail.com>
2015-03-04 15:48:37 +0200

    code leanup

--- app/modules/page_builder/directives/tools_collection.js
+++ app/modules/page_builder/directives/tools_collection.js
@@ -64,3 +64,30 @@ pageBuilder.directive("imageTool", function(){
 		template: "<button class='toolItem imageTool' title='Add Image'>I</button>",
 	}
 });
+
+
+// get template directive functionality. Use: <templates></templates>
+pageBuilder.directive("templates", function(){
+	var linkFn = function(scope, element) {
+		var what = 0; // mockup var
+
+		element.bind("click", function() {
+			// mockup for quick change template
+			if (what) {
+				scope.getPageTemplate('/data/templatePage1.json');
+				what--;
+			} else {
+				scope.getPageTemplate();
+				what++;
+			}
+		});
+	};
+	
+	return {
+		link: linkFn,
+		restrict: "E",
+		template: "<button class='toolItem getTemplate' title='Get Template'>Tpls</button>",
+	}
+});
+
+
