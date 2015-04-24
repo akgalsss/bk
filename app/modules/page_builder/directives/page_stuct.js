19fc0aabe3e0378bb62a12cca889a1a4334f9042
Dnister Lark <dnister.lark@gmail.com>
2015-04-24 09:48:05 +0300

    Drag: fix issue with bubling and calling canDropIn function

--- app/modules/page_builder/directives/page_stuct.js
+++ app/modules/page_builder/directives/page_stuct.js
@@ -43,23 +43,22 @@ bkPageBuilder.directive("rightPanel", function(){
 // draggable directive
 bkPageBuilder.directive('draggable', function() {
   return function(scope, element) {
-      var el = element[0];
+    var el = element[0];
+    el.draggable = true;
+    el.addEventListener('dragstart', function(e) {
+      e.stopPropagation();
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.clearData('Text');
+      e.dataTransfer.setData('Text', this.id);
+      this.classList.add('drag');
+      return false;
+    }, false);
 
-      el.draggable = true;
-
-      el.addEventListener('dragstart', function(e) {
-        e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.clearData('Text');
-        e.dataTransfer.setData('Text', this.id);
-        this.classList.add('drag');
-        return false;
-      }, false);
-
-      el.addEventListener('dragend', function(e) {
-        this.classList.remove('drag');
-        return false;
-      }, false);
-    }
+    el.addEventListener('dragend', function(e) {
+      this.classList.remove('drag');
+      return false;
+    }, false);
+  }
 });
 
 
